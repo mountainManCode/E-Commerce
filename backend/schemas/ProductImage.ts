317679b263a31307575488af2cccde5048becc09
Aaron <aa.griff9@gmail.com>
2021-02-04 16:38:43 -0800

    Setup server side GraphQL.

---
+++ backend/schemas/ProductImage.ts
@@ -0,0 +1,27 @@
+import { list } from '@keystone-next/keystone/schema/dist/keystone.cjs';
+import { text, relationship } from '@keystone-next/fields';
+import { cloudinaryImage } from '@keystone-next/cloudinary';
+import 'dotenv/config';
+
+export const cloudinary = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: 'tailspin',
+};
+
+export const ProductImage = list({
+  fields: {
+    image: cloudinaryImage({
+      cloudinary,
+      label: 'Source',
+    }),
+    altText: text(),
+    product: relationship({ ref: 'Product.photo' }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['image', 'altText', 'product'],
+    },
+  }
+});
