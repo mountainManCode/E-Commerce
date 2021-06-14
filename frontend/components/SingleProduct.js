5a38f6f574a233fc4014bd3f477984b8bdd1e8df
Aaron <aa.griff9@gmail.com>
2021-06-14 15:49:45 -0700

    Created SingleProduct page/component.

---
+++ frontend/components/SingleProduct.js
@@ -0,0 +1,65 @@
+import Head from 'next/head';
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import DisplayError from './ErrorMessage';
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      price
+      description
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function SingleProduct({ id }) {
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: {
+      id,
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+
+  const { Product } = data;
+  return (
+    <ProductStyles>
+      <Head>
+        <title>Tail Spin | {Product.name}</title>
+      </Head>
+      <div className="details">
+        <img
+          src={Product.photo.image.publicUrlTransformed}
+          alt={Product.photo.altText}
+          height="400"
+        />
+        <h2>{Product.name}</h2>
+        <p>{Product.description}</p>
+      </div>
+    </ProductStyles>
+  );
+}
