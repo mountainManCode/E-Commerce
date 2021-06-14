121d09a3515e70efa78a7de95ecadc2c02a0c93a
Aaron <aa.griff9@gmail.com>
2021-06-14 12:25:49 -0700

    Styling of nav/header.

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -23,12 +23,12 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
     display: grid;
-    grid-template-columns: 1fr auto;
+    grid-template-columns: 1.5fr auto;
     border-bottom: 1px solid var(--black, black);
   }
 `;
@@ -40,11 +40,11 @@ export default function Header() {
         <Logo>
           <Link href="/">Tail Spin</Link>
         </Logo>
+        <Nav />
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,31 @@
-import NProgress from 'nprogress';
+import { ApolloProvider } from '@apollo/client';
 import Router from 'next/router';
+import NProgress from 'nprogress';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 import '../components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeStart', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx}) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
