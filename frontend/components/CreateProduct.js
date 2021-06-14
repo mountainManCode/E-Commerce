8544ca7ff00d790833e91c4637300d65dd3a6f27
Aaron <aa.griff9@gmail.com>
2021-06-14 13:49:42 -0700

    Setup Create Product Mutation.

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,30 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Which variables are being passed in? And what types are they.
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -9,13 +34,27 @@ export default function CreateProduct() {
     description: 'Description of product',
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
+
+        // Submit inputfields to the backend.
+        // Can submit the variables here in createProduct(variables), if you don't know the variables til the time of submit.
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="name">
           Name
           <input
@@ -59,9 +98,7 @@ export default function CreateProduct() {
         <button type="button" onClick={resetForm}>
           Reset Form
         </button>
-        <button type="submit" onClick={resetForm}>
-          Add Product
-        </button>
+        <button type="submit">Add Product</button>
       </fieldset>
     </Form>
   );
