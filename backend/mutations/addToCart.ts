1c08cffaf4884f68db29ce6f18a2a7c748d36f06
Aaron <aa.griff9@gmail.com>
2022-01-23 10:22:08 -0800

    Upgrade to Keystone 6

--- backend/mutations/addToCart.ts
+++ backend/mutations/addToCart.ts
@@ -1,13 +1,11 @@
-import { KeystoneContext, SessionStore } from '@keystone-next/types';
-import { CartItem } from '../schemas/CartItem';
+import { KeystoneContext } from '@keystone-6/core/types';
 import { Session } from '../types';
-import { CartItemCreateInput } from '../.keystone/schema-types';
 
 async function addToCart(
 	root: any,
 	{ productId }: { productId: string },
 	context: KeystoneContext
-): Promise<CartItemCreateInput> {
+): Promise<any> {
 	// 1. Query current user and check if signed in.
 	const sesh = context.session as Session;
 
@@ -16,29 +14,29 @@ async function addToCart(
 	}
 
 	// 2. Query the current Users cart
-	const allCartItems = await context.lists.CartItem.findMany({
-		where: { user: { id: sesh.itemId }, product: { id: productId } },
-		resolveFields: 'id,quantity'
+	const allCartItems = await context.db.CartItem.findMany({
+		where: { user: { id: { equals: sesh.itemId } }, product: { id: { equals: productId } } },
+		// resolveFields: 'id,quantity'
 	});
 	const [existingCartItem] = allCartItems;
 
 	if (existingCartItem) {
 		// 3. Check item is in cart.
 		// 4. If it is increment by +1
-		return await context.lists.CartItem.updateOne({
-			id: existingCartItem.id,
+		return await context.db.CartItem.updateOne({
+			where: { id: existingCartItem.id },
 			data: { quantity: existingCartItem.quantity + 1 },
-			resolveFields: false,
+			// resolveFields: false,
 		});
 	}
 
 	// 5. if it is not, create a new item in cart
-	return await context.lists.CartItem.createOne({
+	return await context.db.CartItem.createOne({
 		data: {
 			product: { connect: { id: productId } },
 			user: { connect: { id: sesh.itemId } },
 		},
-		resolveFields: false,
+		// resolveFields: false,
 	});
 }
 
--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -1,4 +1,4 @@
-import { KeystoneContext, SessionStore } from '@keystone-next/types';
+import { KeystoneContext, SessionStore } from '@keystone-6/core/types';
 import { Session } from '../types';
 import { CartItemCreateInput, OrderCreateInput } from '../.keystone/schema-types';
 import { CartItem } from '../schemas/CartItem';
@@ -15,16 +15,16 @@ async function checkout(
 	root: any,
 	{ token }: Arguments,
 	context: KeystoneContext
-): Promise<OrderCreateInput> {
+): Promise<any> {
 	// 1. Make sure they are signed in
 	const userId = context.session.itemId;
 	if(!userId) {
 		throw new Error('You must be signed in to create an order!');
 	}
 	// 2. Query the current User
-	const user = await context.lists.User.findOne({
+	const user = await context.query.User.findOne({
 		where: {id: userId },
-		resolveFields: graphql`
+		query: graphql`
 			id
 			name
 			email
@@ -47,11 +47,13 @@ async function checkout(
 			}
 		`
 	});
+	console.dir(user, { depth: null });
+
 	// Filter out items in Cart with Products that have been deleted.
-	const cartItems = user.cart.filter(cartItem => cartItem.product);
+	const cartItems = user.cart.filter((cartItem: any) => cartItem.product);
 
 	// 3. calc the total price for their order.
-	const amount = cartItems.reduce( (tally: number, cartItem: CartItemCreateInput) => {
+	const amount = cartItems.reduce( (tally: number, cartItem: any) => {
 		return tally + cartItem.quantity * cartItem.product.price;
 	},0);
 	console.log( amount);
@@ -67,7 +69,7 @@ async function checkout(
 	});
 	console.log(charge);
 	// 5.  Convert the cartItems to orderItems
-	const orderItems = cartItems.map(cartItem => {
+	const orderItems = cartItems.map(( cartItem: any ) => {
 		const orderItem = {
 			name: cartItem.product.name,
 			description: cartItem.product.description,
@@ -78,7 +80,7 @@ async function checkout(
 		return orderItem;
 	});
 	// 6. Create the order and return it.
-	const order = await context.lists.Order.createOne({
+	const order = await context.db.Order.createOne({
 		data: {
 			total: charge.amount,
 			charge: charge.id,
@@ -87,12 +89,12 @@ async function checkout(
 		}
 	});
 	// 6. Clean up any old cart item
-	const cartItemIds = user.cart.map(cartItem => cartItem.id);
-	await context.lists.CartItem.deleteMany({
-		ids: cartItemIds
-	});
-
-	return order;
+  const cartItemIds = user.cart.map((cartItem: any) => cartItem.id);
+  console.log('gonna create delete cartItems');
+  await context.query.CartItem.deleteMany({
+    where: cartItemIds.map((id: string) => ({ id })),
+  });
+  return order;
 }
 
 export default checkout;
